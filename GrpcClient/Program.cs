f15f4c98e039fce0c8a956b6b5bcb8e972dc0c67
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2021-02-07 16:21:54 +0300

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
