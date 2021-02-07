73576ea0c351c172d4037968634592358545a2cc
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2021-02-07 20:31:53 +0300

    BiDirectiaonal added

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,24 +1,54 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using GrpcMessageService;
 using GrpcService;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace GrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var channel = GrpcChannel.ForAddress("https://localhost:5001/");
             //var greetClient = new Greeter.GreeterClient(channel);
             //var a =  greetClient.SayHello(new HelloRequest() { Name = "Furkan" });
             var messageClient = new Message.MessageClient(channel);
-            var result = messageClient.SendMessage(new MessageRequest
+            //var result = messageClient.SendMessageStream(new MessageRequest
+            //{
+            //    Message = "Naber",
+            //    Name = "Furkan"
+            //});
+            //CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            //while ( result.ResponseStream.MoveNext(cancellationTokenSource.Token).Result)
+            //{
+            //    System.Console.WriteLine(result.ResponseStream.Current.Message);
+            //}
+            //var messageClient = new Message.MessageClient(channel);
+
+            //var request = messageClient.SendMessageStreamFromClient();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //   await request.RequestStream.WriteAsync(new MessageRequest { Name = "Furkan", Message = "Naber" });
+            //}
+            //await request.RequestStream.CompleteAsync();
+
+            //var response =await  request.ResponseAsync;
+            //Console.WriteLine(response.Message);
+            //Console.Read();
+            var result = messageClient.SendMessageStreamBiDirectioanal();
+          
+                for (int i = 0; i < 10; i++)
+                {
+                    await result.RequestStream.WriteAsync(new MessageRequest { Name = "Furkan", Message = "Naber" });
+                }
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            while (await result.ResponseStream.MoveNext(cancellationTokenSource.Token))
             {
-                Message = "Naber",
-                Name = "Furkan"
-            });
-            Console.WriteLine(result.Message);
+                System.Console.WriteLine(result.ResponseStream.Current.Message);
+            }
             Console.Read();
         }
     }
